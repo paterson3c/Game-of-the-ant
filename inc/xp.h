52f02200391d9ebbf496efee717f623774065b6c
kunn04 <javieraguero104@gmail.com>
2023-05-06 17:27:23 +0200

    documentación

--- inc/xp.h
+++ inc/xp.h
@@ -34,29 +34,88 @@ XP *xp_create();
  */
 STATUS xp_destroy(XP *xp);
 
-
-
+/**
+ * @brief It sets the xp
+ * 
+ * @param xp the xp to set
+ * @param value the value to set
+ * @return STATUS the status of the function
+ */
 STATUS xp_setXp(XP *xp, int value);
 
-
+/**
+ * @brief It gets the xp
+ * 
+ * @param xp the xp to get
+ * @return int the xp
+ */
 int xp_getXp(XP *xp);
 
-
+/**
+ * @brief It sets the level
+ * 
+ * @param xp the xp to set
+ * @param value the value to set
+ * @return STATUS the status of the function (ERROR, OK)
+*/
 STATUS xp_setLevel(XP *xp, int value);
 
-
+/**
+ * @brief It gets the level
+ * 
+ * @param xp the xp to get
+ * @return int the level
+ */
 int xp_getLevel(XP *xp);
 
+/**
+ * @brief It gets the xp
+ * 
+ * @param xp the xp to set
+ * @return STATUS the status of the function (ERROR, OK)
+*/
 int xp_getXp(XP *xp);
 
+/**
+ * @brief It levels up 
+ * 
+ * @param xp Pointer to the structure containing the level of the player
+ * @return STATUS the status of the function (ERROR, OK)
+*/
 STATUS xp_levelUp(XP *xp);
 
+/**
+ * @brief It adds xp to the player
+ * 
+ * @param xp Pointer to the structure containing the level of the player
+ * @param value the value to add
+ * @return STATUS the status of the function (ERROR, OK)
+*/
 STATUS xp_addXp(XP *xp, int value);
 
+/**
+ * @brief It sets the max xp
+ * 
+ * @param xp Pointer to the structure containing the level of the player
+ * @param value the value to set
+ * @return STATUS the status of the function (ERROR, OK)
+*/
 STATUS xp_setMaxXp(XP *xp, int value);
 
+/**
+ * @brief It gets the max xp
+ * 
+ * @param xp Pointer to the structure containing the level of the player
+ * @return STATUS the status of the function (ERROR, OK)
+*/
 STATUS xp_getMaxXp(XP *xp);
 
+/**
+ * @brief It prints the xp
+ * 
+ * @param xp Pointer to the structure containing the level of the player
+ * @return STATUS the status of the function (ERROR, OK)
+*/
 STATUS xp_print(XP *xp);
 
 
